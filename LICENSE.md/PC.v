c2a7c3adf1a65880c8547a57c81eabe2f16e226f
DrunkFishes <33043086+DrunkFishes@users.noreply.github.com>
2017-11-20 17:42:40 -0800

    Add files via upload

---
+++ LICENSE.md/PC.v
@@ -0,0 +1,52 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:43:56 11/17/2017 
+// Design Name: 
+// Module Name:    PC 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 1ps
+ /*******************************************************************************
+ * Author:   Steven Phan
+ * Email:	 
+ * Filename: 
+ * Date:     October 23, 2017
+ * Version:  1.0
+ * Purpose:  
+ *					
+ * Notes:	 
+ *******************************************************************************/
+module PC(clk, reset, ld, Din, DA, DB, oeA, oeB);
+
+	input 		   clk, reset, ld, oeA, oeB;
+	input  [15:0]  Din;
+	output [15:0]  DA, DB;
+	reg    [15:0]  Dout;
+	
+// behavioral section for writing to the regisdter
+always@ (posedge clk or posedge reset)
+	if (reset)
+		Dout <= 16'b0;
+	else
+		if (ld)
+				Dout <= Din;
+		else  Dout <= Dout;
+	
+// conditional continout assignments for reading th3e register
+assign DA = oeA ? Dout : 16'hz;
+assign DB = oeB ? Dout : 16'hz;
+
+endmodule
---
+++ LICENSE.md/Reg_16.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:02:40 11/17/2017 
+// Design Name: 
+// Module Name:    Reg_16 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Reg_16(clk, reset, ld, count, Din, Dout);
+
+input clk, reset, ld, count;
+input [15:0] Din;
+output reg [15:0] Dout;
+
+always @ (posedge clk or posedge reset)
+	if(reset == 1'b1)
+		Dout <= 16'b0;
+	else
+		case({ld,count})
+			2'b10: Dout <= Din;
+			2'b01: Dout <= Din +16'b1;
+			default: Dout <= Dout;
+		endcase
+endmodule
