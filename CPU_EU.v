fd850859b1415d06e156a08aeaa43e7d605f00e2
btadinata <33048452+btadinata@users.noreply.github.com>
2017-11-24 15:10:29 -0800

    Add files via upload

--- CPU_EU.v
+++ CPU_EU.v
@@ -29,16 +29,14 @@ module CPU_EU(clk, reset, Din, we, addr_sel, sel, ld_pc, count_pc, ld_ir,
 	
 	wire [15:0] Reg_out, Alu_out, ir_Q, pc_Q;
 	
-									//(clk, reset, W_En, S_Sel, W_Adr,     R_Adr,     S_Adr, Alu_  Op,         DS, 
-	integer_datapath  IDCP	  (clk, reset, W_EN, sel,   ir_Q[8:6], ir_Q[8:6], ir_Q[2:0],   ir_Q[15:12],Din,
+							//(clk, reset, W_En, S_Sel,   W_Adr  ,   R_Adr  ,   S_Adr  ,   Alu_  Op ,   DS, 
+	integer_datapath  IDCP	  (clk, reset, W_EN,  sel , ir_Q[8:6], ir_Q[8:6], ir_Q[2:0], ir_Q[15:12],  Din,
+							// C, N, Z, Reg_Out, Alu_Out);
+							   C, N, Z, Reg_out,  Dout);
 	
-									//C, N, Z, Reg_Out, Alu_Out);
-									 C, N, Z, Reg_out,  Dout);
-	
-	
-					// (clk, reset, ld,    count,    Din,  Dout)
+			  // (clk, reset,   ld ,   count ,  Din, Dout)
 	Reg_16   PC  (clk, reset, ld_pc, count_pc, Dout, pc_Q);
-	Reg_16   IR  (clk, reset, ld_ir, count_pc, Din,  ir_Q);
+	Reg_16   IR  (clk, reset, ld_ir, count_pc,  Din, ir_Q);
 	
 	
 	assign Addr_out = (addr_sel)? Reg_out: pc_Q;
--- lab7_toplvl.v
+++ lab7_toplvl.v
@@ -33,16 +33,13 @@ module lab7_toplvl(clk, reset, step, mem_w, w_en, addr_sel, s_sel, pc_ld, pc_inc
     one_shot    btd(step, clk500_out, reset, step_clk);
     one_shot    btl(mem_w, clk500_out, reset, memw_en);
     
-    //CPU_EU         (clk, reset, Din, we, addr_sel, sel, ld_pc, count_pc, ld_ir, 
-	//               C, N, Z, Addr_out, Dout)
-    CPU_EU      cpu(clk, reset, Addr_Dout, w_en, addr_sel, s_sel, pc_ld, pc_inc, ir_ld, C, N, Z, CPU_Addr, CPU_Dout);
+    //CPU_EU       (clk, reset,     Din  ,  we , addr_sel,  sel , ld_pc, count_pc, ld_ir, C, N, Z, Addr_out,   Dout  )
+    CPU_EU      cpu(clk, reset, Addr_Dout, w_en, addr_sel, s_sel, pc_ld,  pc_inc , ir_ld, C, N, Z, CPU_Addr, CPU_Dout);
     
-    //ram   (clk, we, [15:0]addr,[15:0] din,[15:0] dout)
+    //ram          (clk, we, [15:0]addr,[15:0] din,[15:0] dout)
     ram         mem(clk, memw_en, CPU_Addr, CPU_Dout, Addr_Dout);
     
-    //display_controller(clk, reset, An, D, a, b, c, d, e, f, g)
-    display_controller      display(clk, reset, An, {CPU_Addr, CPU_Dout}, 
-                                    a, b, c, d, e, f, g);
-
+    //display_controller           (clk, reset, An,           D         , a, b, c, d, e, f, g)
+    display_controller      display(clk, reset, An, {CPU_Addr, CPU_Dout}, a, b, c, d, e, f, g);
 
 endmodule
